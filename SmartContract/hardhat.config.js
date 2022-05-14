eec34d8987f0ede63dcf0829002931897239c91a
parth1007 <parthsoni03ps@gmail.com>
2022-05-15 00:09:09 +0530

    files removed

---
+++ SmartContract/hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomiclabs/hardhat-waffle");
+
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/rUQdS3tjPgQ7OB8BMk3REUojKd3oB41d",
+      accounts: ["bd1325473b71d252c4c76415b6cf3f286997198e9235883f57498fd8def6f9e1"]
+    },
+  },
+};
+
+// ***deprecated
+// Deploying contracts with account:  0x762c97D45666d08C490954E07749F024cDB0F616
+// Account balance:  100000000000000000
+// WavePortal address:  0x686D24cF80E9D10e952Ba0F0Eda0a85393B14898
+
+// ***deprecated
+// Deploying contracts with account:  0x762c97D45666d08C490954E07749F024cDB0F616
+// Account balance:  98815257410509520
+// WavePortal address:  0xfC940abC5aCa54E5622f4f29E3B31C4B0BB7A1cE
+
+
+//0xD73E03Ad1f004F653F674B1b67eD403c181Ba040
