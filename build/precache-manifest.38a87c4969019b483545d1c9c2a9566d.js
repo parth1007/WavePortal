a81f24bb29ffbd17542871cfb4e32e824318d0ca
parth1007 <parthsoni03ps@gmail.com>
2022-05-15 12:03:38 +0530

    build added

---
+++ build/precache-manifest.38a87c4969019b483545d1c9c2a9566d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf405f9f9f23ed08c6c77c60cd11d5d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2994832322fd4cd3592",
+    "url": "/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "26db0a731cf1cf092efe",
+    "url": "/static/css/main.5fdc96be.chunk.css"
+  },
+  {
+    "revision": "d2994832322fd4cd3592",
+    "url": "/static/js/2.1cdaf316.chunk.js"
+  },
+  {
+    "revision": "26db0a731cf1cf092efe",
+    "url": "/static/js/main.bc81dcac.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
